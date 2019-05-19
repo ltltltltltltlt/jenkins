7dd302aa5a398a336358fded0f6efaf7c53a84e8
e <lijinming0214@126.com>
2019-05-19 14:00:58 +0800

    commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public String sayHello(){ 
 	//修改返回的字符串的内容 
 	
-	Syetem.out.println("10161733. Hello jenkins"); 
+	Syetem.out.println("10161733ljm. Hello jenkins"); 
 	} 
 }
